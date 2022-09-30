970e765f27dde6278dcc782cfc44fc1561a03132
ahmedzoha029 <57677449+ahmedzoha029@users.noreply.github.com>
2022-09-30 04:11:02 -0600

    codeWithMosh utilized createSlice

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,27 +1,29 @@
 import * as toolkitRaw from '@reduxjs/toolkit';
-const { createAction, createReducer } = toolkitRaw.default ?? toolkitRaw;
+const { createSlice } = toolkitRaw.default ?? toolkitRaw;
 
-export const bugAdded = createAction("bugAdded");
-export const bugResolved = createAction("bugResolved");
-export const bugRemoved = createAction("bugRemoved");
-
-
-//Reducers
 let lastId = 0;
 
-export default createReducer([], {
-  [bugAdded.type]: (state, action) => {
-    state.push({
-      id: ++lastId,
-      description: action.payload.description,
-      isResolved: false
-    })
-  },
-  [bugResolved.type]: (state, action) => {
-    const bugIndex= state.findIndex(bug => bug.id === action.payload.id);
-    state[bugIndex].isResolved = true
-  },
-  [bugRemoved.type]: (state, action) => {
-    state.filter(bug => bug.id !== action.payload.id)
+const bugsSlice = createSlice({
+  name: 'bugs',
+  initialState: [],
+  reducers: {
+    bugAdded: (state, action) => {
+      state.push({
+        id: ++lastId,
+        description: action.payload.description,
+        isResolved: false
+      })
+    },
+    bugResolved: (state, action) => {
+      const bugIndex= state.findIndex(bug => bug.id === action.payload.id);
+      state[bugIndex].isResolved = true
+    },
+    bugRemoved: (state, action) => {
+      state.filter(bug => bug.id !== action.payload.id)
+    }
   }
-});
+})
+
+export const { bugAdded, bugResolved, bugRemoved } = bugsSlice.actions
+export default bugsSlice.reducer
+

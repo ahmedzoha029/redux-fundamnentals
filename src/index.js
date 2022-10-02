20b2f6dc52e818bcac7ac704ee47785018c48ce8
ahmedzoha029 <57677449+ahmedzoha029@users.noreply.github.com>
2022-10-01 21:24:50 -0600

    codeWithMosh middlewares

--- src/index.js
+++ src/index.js
@@ -9,11 +9,16 @@ const unsubscribe = store.subscribe(()=> {
   console.log('store changed', store.getState());
 })
 
-store.dispatch(bugAdded({description :"Bug 1 Added" }));
-store.dispatch(bugAdded({description :"Bug 2 Added" }));
-store.dispatch(bugAdded({description :"Bug 3 Added" }));
-store.dispatch(bugResolved( { id: 1 }));
+store.dispatch((dispatch, getState) => {
+  dispatch({ type: "error", message: "error"});
+  console.log('state after api call', getState());
+})
 
-store.dispatch(addProject({ projectName: "Redux Project"}));
-getUnresolvedBugs(store.getState());
+//store.dispatch(bugAdded({description :"Bug 1 Added" }));
+// store.dispatch(bugAdded({description :"Bug 2 Added" }));
+// store.dispatch(bugAdded({description :"Bug 3 Added" }));
+// store.dispatch(bugResolved( { id: 1 }));
+
+// store.dispatch(addProject({ projectName: "Redux Project"}));
+//getUnresolvedBugs(store.getState());
 
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,17 @@
 import * as toolkitRaw from '@reduxjs/toolkit';
-const { configureStore } = toolkitRaw.default ?? toolkitRaw;
+const { configureStore, getDefaultMiddleware } = toolkitRaw.default ?? toolkitRaw;
+import func from './middlewares/func.js'
+import logger from './middlewares/logger.js';
+import toast from './middlewares/toast.js';
 import reducer from './reducer.js'
 
 export default function () {
-  return configureStore({ reducer });
+  return configureStore({
+    reducer,
+    middleware: [
+      ...getDefaultMiddleware(),
+      logger({development: 'console' }),
+      toast
+    ]
+  });
 }
